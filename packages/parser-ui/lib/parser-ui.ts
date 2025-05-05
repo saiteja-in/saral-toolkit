2314af0c5674a6bc8e0d3ac192b53ba7baadf802
saiteja-in <vurukondasaiteja13@gmail.com>
2025-05-05 11:25:31 +0530

    add DocumentViewer and ParsedResultsPanel components for document display and interaction

--- packages/parser-ui/lib/parser-ui.ts
+++ packages/parser-ui/lib/parser-ui.ts
@@ -1,4 +1,14 @@
-import { isOdd } from "core";
-export function isEven(n:number):boolean{
-  return isOdd(n) === false;
-}
+// Export utilities
+export { isEven, formatPercentage, truncateText } from './utils/helpers';
+
+// Export types
+export * from './types';
+
+// Export components
+export { DocumentUploader } from './components/DocumentUploader';
+export { DocumentViewer } from './components/DocumentViewer';
+export { ParsedResultsPanel } from './components/ParsedResultsPanel';
+export { DocumentParser } from './components/DocumentParser';
+
+// Export services
+export { ParserService, parserService } from './services/ParserService';
